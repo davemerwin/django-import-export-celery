8a24f8519411306c4121d2c1187de0396ec7fa85
Dave Merwin <dave@davemerwin.com>
2020-05-26 11:44:10 -0700

    tweaking how the name is included

--- import_export_celery/__init__.py
+++ import_export_celery/__init__.py
@@ -1,1 +1,6 @@
-from project.celery import app as celery_app
+from django.conf import settings
+import importlib
+
+celery_module = settings.get('CELERY_INIT_MODULE', 'project.celery')
+# from settings.DIEC_NAME import app as celery_app
+celery_app = importlib.import_module(celery_module).app
