47e7c2f311f2bad9ffb27c8012803e87de7822c7
Dave Merwin <dave@davemerwin.com>
2020-05-26 12:01:12 -0700

    using gett attr

--- import_export_celery/__init__.py
+++ import_export_celery/__init__.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 import importlib
 
-celery_module = settings.CELERY_INIT_MODULE
+celery_module = getattr(settings, 'IMPORT_EXPORT_CELERY_INIT_MODULE', 'project.celery')
 # from settings.DIEC_NAME import app as celery_app
 celery_app = importlib.import_module(celery_module).app
